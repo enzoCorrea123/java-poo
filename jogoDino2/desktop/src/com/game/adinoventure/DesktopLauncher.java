bdce296d6ce5302b5cd53f9569e9586504bc4976
Enzo322 <enzorufino1508@gmail.com>
2023-11-02 14:11:33 -0300

    novo gradle

--- jogoDino2/desktop/src/com/game/adinoventure/DesktopLauncher.java
+++ jogoDino2/desktop/src/com/game/adinoventure/DesktopLauncher.java
@@ -3,15 +3,13 @@ package com.game.adinoventure;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.game.adinoventure.Adinoventure;
-import com.game.adinoventure.dino.Dino;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setWindowedMode(800, 480);//resolução HD
-		config.setTitle("Adinoventure");
-		new Lwjgl3Application(Adinoventure.getInstance(), config);
-		//oi Enzo.
+		config.setForegroundFPS(60);
+		config.setTitle("jogoDino2");
+		new Lwjgl3Application(new Adinoventure(), config);
 	}
 }
